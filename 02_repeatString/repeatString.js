2251f9f2425968db1cbaf5003ba4889f18646678
PierCommando <57146498+PierCommando@users.noreply.github.com>
2022-12-19 17:57:40 +0100

    ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(str, n) {
+    let repeatedString = ""
+    if (n < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < n; i++) {
+            repeatedString += str
+        } 
+    return repeatedString
+    };
+}
 // Do not edit below this line
 module.exports = repeatString;
