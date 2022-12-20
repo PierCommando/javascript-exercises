993713bc8fbebcca5caf3d07ed18e914e93aa072
PierCommando <57146498+PierCommando@users.noreply.github.com>
2022-12-20 14:25:55 +0100

    ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0
+    if (x < 0 || y < 0 || typeof x === 'string' || typeof y === 'string' || isNaN(x) || isNaN(y)) return "ERROR"
+    let i = (x < y) ? x : y
+    const j = (x < y) ? y : x
+    for (; i<j+1; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
