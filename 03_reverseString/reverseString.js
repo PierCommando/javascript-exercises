691a97d2514a90e467f01c1bc50c2150a2729f65
PierCommando <57146498+PierCommando@users.noreply.github.com>
2022-12-20 12:30:42 +0100

    ex 03 and ex 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArray = str.split("");
+    const arrayReversed = strArray.reverse()
+    const strReversed = arrayReversed.join("")
+    return strReversed
 };
 
 // Do not edit below this line
