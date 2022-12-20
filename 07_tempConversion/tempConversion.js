fd2f51ff3f671d9b0cb53f5c52ec35b626c4e399
PierCommando <57146498+PierCommando@users.noreply.github.com>
2022-12-20 15:22:23 +0100

    ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  let converted = (n - 32) * (5/9)
+  let rounded = Math.round(converted * 10)/10
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  let converted = (n * (9/5) + 32)
+  let rounded = Math.round(converted * 10)/10
+  return rounded
 };
 
 // Do not edit below this line
